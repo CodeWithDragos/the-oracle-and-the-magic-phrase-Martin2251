1b9aaa65e7ecff36028459a1d62f9e546f0ead04
martin lowe <martinlowe673@gmail.com>
2021-03-01 19:23:45 +0000

    playing with magic button

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-const generateBtn = document.querySelector("#magic-btn");
+const generateBtn = document.querySelector("#btn-flat");
 
 function makeSomeMagic() {
   const likesSports = confirm("Do you like to do sports?");
   const likesPuzzles = confirm("Do you like to make puzzles?");
   const likesPets = confirm("Do you like pets?");
-  const likesPizza = confirm("Do you like pizza?");
-  const likesMusic =confirm("Do you like music ?");
-  const likesChocolate = confirm("Do you like Chocolate?");
+  const likesPizza = confirm("Do you like piza?");
 
- if likesSports & likePuzzles & likesPets & likesPizza & likesMusic & likesChocolatelikesSports & likesPets & likesSports
-
- else
-
-
+  if (likesPuzzles && !likesSports && !likesPets && likesPizza) {
+    return "You will make a great developer my friend. Just keep on coding!";
+  } else {
+    return "dont worry my friend if Donald Trump can make it" :)";
+  }
 }
 
 function displayMagicPhrase() {
   const magicPhrase = makeSomeMagic();
-  const magicPhraseDisplay = document.querySelector("#magic-phrase");
+  const magicPhraseDisplay = document.querySelector("#magic-phase");
 
   magicPhraseDisplay.value = magicPhrase;
 }
 
-// Attach event listener to the magi button
+// Attach event listener to the magic button
 generateBtn.addEventListener("click", displayMagicPhrase);
-
