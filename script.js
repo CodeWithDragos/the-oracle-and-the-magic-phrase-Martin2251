1dd69b9cbb93abf83c08c877f0f87cfbf0cc3b73
martin lowe <martinlowe673@gmail.com>
2021-03-02 08:28:44 +0000

    added fonts and restyled css

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const generateBtn = document.querySelector("#btn-flat");
+// Use the skeleton bellow as a basis for your script
+var generateBtn = document.querySelector("#magic-btn");
 
 function makeSomeMagic() {
-  const likesSports = confirm("Do you like to do sports?");
-  const likesPuzzles = confirm("Do you like to make puzzles?");
-  const likesPets = confirm("Do you like pets?");
-  const likesPizza = confirm("Do you like pizza?");
+  var youLikeComputers = confirm("Do you like computers?");
+  var youLikeSport = confirm("Do you like to do sports?");
+  var youLikePuzzles = confirm("Do you like to solve puzzles?");
+  var youLikePets = confirm("Do you love pets?");
+  var youLikePizza = confirm("Do you eat Pizza?");
+  var youlikeDancing = confirm("Do you like Dancing?");
 
-  if (likesPuzzles && !likesSports && !likesPets && likesPizza) {
+  if (
+    youLikeComputers &&
+    !youLikeSport &&
+    youLikePuzzles &&
+    !youLikePets &&
+    !youLikePizza &&
+    !youlikeDancing
+  ) {
     return "You will make a great developer my friend. Just keep on coding!";
   } else {
-    return "dont worry my friend if Donald Trump can make it" :)";
+    return "Your future is doomed. But hey you can always try again :)";
   }
 }
 
 function displayMagicPhrase() {
-  const magicPhrase = makeSomeMagic();
-  const magicPhraseDisplay = document.querySelector("#magic-phase");
+  var magicPhrase = makeSomeMagic();
+  var magicPhraseDisplay = document.querySelector("#magic-phrase");
 
   magicPhraseDisplay.value = magicPhrase;
 }
